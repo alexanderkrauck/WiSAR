5d0a2ab68548dc0ee69b5d170c12068546a980d6
Alexander Krauck <alexander.krauck@gmail.com>
2021-12-06 00:49:48 +0100

    TimestepAnomalyDetection

--- utils/architectures.py
+++ utils/architectures.py
@@ -23,6 +23,36 @@ class BasicConvolutionalAnomalyDetection:
         for sample in samples:
 
             photos = sample.photos
+            
+            #TODO:
+            #Step 1: Select grid from all images
+            #Step 2: Put each grid-image into the autoencoder that is passed
+            #Step 3: Put the reconstructed images back together (to the original time-view-structure)
 
-            pass  # TODO: DO EVERYTHING!
+            #Step a: Take Differences between original and reconstructed
+            #Step b: Differences between timesteps
+            #Step c: Warp
+            #Step d: Combine Timesteps
+
+            #Step 4: Threshhold for bounding boxes
+            #Step 5: Output into the files/return values.
+
+
+            pass
+
+class BasicTimestepAnomalyDetection:
+    def __init__(self, pretrained_convolutional_network):
+
+        self.pretrained_convolutional_network = pretrained_convolutional_network
+
+        pass
+
+    def infer(self, samples: List[MultiViewTemporalSample]):
+
+        for sample in samples:
+
+            photos = sample.photos
+            
+
+            pass
 
